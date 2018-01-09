622fa7740d0bd7da3d6c4024a303e3b0f2ec85a4
Oliver Haack <zccoliver@gmail.com>
2018-01-09 19:12:07 +0100

    Express Generator REST API

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
